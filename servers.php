efa0a316ee0a47b88544ab855cafd5af84135cf2
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-15 17:52:29 -0700

    Added servers.php

---
+++ servers.php
@@ -0,0 +1,27 @@
+<?php
+    include('lib/sql.php');
+    $sqlquery = new doSQL();
+    $sqlquery->doSQLStuff("SELECT * FROM `Servers` WHERE fingerprint ='".$_GET['fingerprint']."'");
+    $names = $sqlquery->get_names();
+    $ports = $sqlquery->get_ports();
+    $fingerprints = $sqlquery->get_fingerprints();
+    $id = $sqlquery->get_id();
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <title>craftback</title>
+    </head>
+    <body>
+        <!-- Shamelessly snatched from W3 -->
+        <div class="topnav" id="myTopnav">
+            <a href="#home" class="active">Home</a>
+            <a href="#about">About</a>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()">&#9776;</a>
+        </div>
+        <iframe src="http://games01-serv:<?php echo $ports[0]; ?>/getLog"></iframe>
+    </body>
+</html>
