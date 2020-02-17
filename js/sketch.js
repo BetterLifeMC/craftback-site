cb1be75ac320c603994d7e0eadb2ed384db42f11
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-17 08:57:08 -0700

    Working on darkmode button.

---
+++ js/sketch.js
@@ -0,0 +1,6 @@
+function toggleDarkMode(){
+    if(document.cookie == "darkmode"){
+        document.getElementsByTagName("body")[0].style.backgroundColor = "#111";
+        document.getElementsByTagName("body")[0].style.color = "#ddd";
+    }
+}
