cbda500621f51d33847aba987f7d4d56bcb5e050
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-16 19:24:39 -0700

    Undo FA changes.

--- servers.php
+++ servers.php
@@ -128,7 +128,6 @@
     <body onload="getChats();getPlayerInfo();">
         <div class="w3-bar w3-metro-darken">
             <a href="./index.php" class="w3-button w3-green w3-bar-item">Home</a>
-            <i class='fas fa-adjust'></i>
             <div class="w3-dropdown-hover">
                 <button class="w3-button">Git</button>
                 <div class="w3-dropdown-content w3-bar-block w3-card-4">
@@ -136,7 +135,6 @@
                     <a href="https://gitlab.com/gt3ch1/craftback-site" class="w3-bar-item w3-button" >CraftBack UI</a>
                 </div>
             </div>
-            <button class="w3-button"><i class="fas fa-adjust">
         </div>
 
         <div id="main" class="w3-row w3-animate-opacity" style="width=300px;">
