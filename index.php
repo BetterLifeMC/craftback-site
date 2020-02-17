dd2957240b1aca46c8cad5aa9fe7b7bc238736d6
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-16 19:33:20 -0700

    Added FA to home page.

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-metro.css">
         <title>CraftBack</title>
+        <script src="https://kit.fontawesome.com/1be2cd7175.js" crossorigin="anonymous"></script>
+
     </head>
     <body>
         <!-- Shamelessly snatched from W3 -->
