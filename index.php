de2a1fd06be6ae2909660ee9b1f25e923cc0f783
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-17 08:58:12 -0700

    Forgot to get the script.

--- index.php
+++ index.php
@@ -16,9 +16,10 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css">
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">\
         <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-metro.css">
         <title>CraftBack</title>
+        <script src="js/sketch.js"></script>
         <script src="https://kit.fontawesome.com/1be2cd7175.js" crossorigin="anonymous"></script>
 
     </head>
