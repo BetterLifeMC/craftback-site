25b0135616eab445fcccdcaf967f6792846b7590
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-17 09:03:51 -0700

    More darkmode work.

--- js/sketch.js
+++ js/sketch.js
@@ -2,5 +2,9 @@ function toggleDarkMode(){
     if(document.cookie == "darkmode"){
         document.getElementsByTagName("body")[0].style.backgroundColor = "#111";
         document.getElementsByTagName("body")[0].style.color = "#ddd";
+        var length = document.getElementsByClassName("info").length;
+        for (var i = 0; i < length; i++) {
+            document.getElementsByClassName("info")[i].style.border = "1px solid #999";
+        }
     }
 }
