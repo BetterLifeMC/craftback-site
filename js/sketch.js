84b3fc41bda80c9a52fc1f4a62a2ca7e65585db7
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-21 15:37:58 -0700

    This shall eventually work.

--- js/sketch.js
+++ js/sketch.js
@@ -57,7 +57,8 @@ function loadServerInfo(HostNameArray, PortArray){
                 200: function (response) {
                     // response = response.replace("[","[\"");
                     // response = response.replace("]","\"]");
-                    response = response.replace(",","\",\"")
+                    // response = response.replace(",","\",\"")
+                    response.split(",");
                     results.push(response);
                     console.log(response);
                 }
