afb3e6c6012dedeeffa485fb1d6d7a5112d2294c
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-22 13:02:33 -0700

    Changed order for dark mode toggle

--- js/sketch.js
+++ js/sketch.js
@@ -3,13 +3,14 @@ function loadDarkMode(){
         document.getElementsByTagName("body")[0].style.backgroundColor = "#111";
         document.getElementsByTagName("body")[0].style.color = "#ddd";
         var length = document.getElementsByClassName("info").length;
-        for (var i = 0; i < length; i++) {
-            document.getElementsByClassName("info")[i].style.border = "1px solid #999";
-        }
         if(document.getElementsByTagName("input")[0] != undefined){
             document.getElementsByTagName("input")[0].style.backgroundColor = "#333"
             document.getElementsByTagName("input")[0].style.color = "#aaa";
         }
+        for (var i = 0; i < length; i++) {
+            document.getElementsByClassName("info")[i].style.border = "1px solid #999";
+        }
+
         length = document.getElementsByTagName("a").length;
         for (var i = 0; i < length; i++) {
             document.getElementsByTagName("a")[i].style.backgroundColor = "#111";
@@ -29,13 +30,14 @@ function loadDarkMode(){
         document.getElementsByTagName("body")[0].style.backgroundColor = "#fff";
         document.getElementsByTagName("body")[0].style.color = "#000";
         var length = document.getElementsByClassName("info").length;
-        for (var i = 0; i < length; i++) {
-            document.getElementsByClassName("info")[i].style.border = "1px solid #fff";
-        }
         if(document.getElementsByTagName("input")[0] != undefined){
             document.getElementsByTagName("input")[0].style.backgroundColor = "#fff";
             document.getElementsByTagName("input")[0].style.color = "#000";
         }
+        for (var i = 0; i < length; i++) {
+            document.getElementsByClassName("info")[i].style.border = "1px solid #fff";
+        }
+
         length = document.getElementsByTagName("a").length;
         for (var i = 0; i < length; i++) {
             document.getElementsByTagName("a")[i].style.backgroundColor = "#fff";
