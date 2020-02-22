886fce932591b076f0f230df51777498198309ee
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-21 17:06:02 -0700

    Remove from body onload

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <script src="https://kit.fontawesome.com/1be2cd7175.js" crossorigin="anonymous"></script>
 
     </head>
-    <body onload="loadDarkMode();loadServerInfo(serverHostnames, serverPorts);">
+    <body onload="loadDarkMode();">
         <!-- Shamelessly snatched from W3 -->
         <div>
             <div class="w3-bar w3-metro-darken">
