52b72d3325d1e5c7adf4107730829c6c20a2bead
GT3CH1 <gcpease@gt3ch1.tk>
2020-02-22 00:48:48 -0700

    Removed weird jquery fadein on index.php

--- index.php
+++ index.php
@@ -25,9 +25,7 @@
             var serverPorts = ["<?php echo implode('","', $ports); ?>"];
             var serverHostnames = ["<?php echo implode('","', $hostnames) ?>"];
             var results = [];
-            $( document ).ready(function() {
-                $('#bigcontainer').fadeIn(800);
-            });
+
         </script>
         <script src="https://kit.fontawesome.com/1be2cd7175.js" crossorigin="anonymous"></script>
 
@@ -58,7 +56,7 @@
                 <button onclick="toggleDarkMode();" class="w3-button" style="float:right;"><i class="fas fa-adjust"></i></button>
             </div>
         </div>
-        <div id="bigcontainer" style="display: none;">
+        <div id="bigcontainer">
 
         <?php
             for($i = 0; $i < sizeof($id); $i++){
