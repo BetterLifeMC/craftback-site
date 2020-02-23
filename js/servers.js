05f7ca9690401a752ec975267848461fe5fb4a5e
Gavin C. Pease <gcpease@gt3ch1.tk>
2020-02-23 10:47:18 -0700

    Rename servers.php to servers.html

--- js/servers.js
+++ js/servers.js
@@ -54,3 +54,110 @@ function addServersToMainPage(){
         }
     }
 }
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null){
+       return null;
+    }
+    else{
+       return results[1] || 0;
+    }
+}
+function getChats(){
+    index = serverFingerPrints.indexOf($.urlParam("fingerprint"));
+    console.log("Server fingerprint index: " + index);
+    $.get({
+        url: 'http://<?php echo $hostnames[0]; ?>:<?php echo $ports[0]; ?>/getLog',
+        dataType: 'text',
+        type: 'GET',
+        async: true,
+        statusCode: {
+            404: function (response) {
+                alert(404);
+            },
+            200: function (response) {
+                document.getElementById("logConsole").innerHTML = response;
+            }
+        },
+        error: function (jqXHR, status, errorThrown) {
+        }
+    });
+    setTimeout(getChats, 1000);
+}
+$(function () {
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'get',
+            url: 'http://<?php echo $hostnames[0]; ?>:<?php echo $ports[0]; ?>/sendMessage/',
+            data: $('form').serialize(),
+            success: function () {
+                document.getElementById('messageBox').value="";
+            }
+        });
+    });
+    document.getElementById('messageBox').value="";
+ });
+
+var playerUUIDList;
+var playerNameList;
+var currentPlayerCount;
+var playerArrayUUIDList;
+var PlayerArrayNameList;
+
+function getPlayerInfo(){
+    $.get({
+        url: 'http://<?php echo $hostnames[0]; ?>:<?php echo $ports[0]; ?>/getPlayerUUIDS',
+        dataType: 'text',
+        type: 'GET',
+        async: true,
+        statusCode: {
+            404: function (response) {
+                alert(404);
+            },
+            200: function (response) {
+                playerUUIDList = JSON.parse(response);
+
+                currentPlayerCount = playerUUIDList.length;
+            }
+        },
+        error: function (jqXHR, status, errorThrown) {
+        }
+    });
+
+    $.get({
+        url: 'http://<?php echo $hostnames[0]; ?>:<?php echo $ports[0]; ?>/getPlayerNames',
+        dataType: 'text',
+        type: 'GET',
+        async: true,
+        statusCode: {
+            404: function (response) {
+                alert(404);
+            },
+            200: function (response) {
+                playerNameList = JSON.parse(response);
+
+            }
+        },
+        error: function (jqXHR, status, errorThrown) {
+        }
+    });
+
+    document.getElementById("allPlayers").innerHTML = "";
+    try{
+        if(playerUUIDList[0].length > 0){
+            for (var i = 0; i < currentPlayerCount; i++) {
+                document.getElementById("allPlayers").innerHTML += "<span id='playerHead'>" + playerNameList[i]+"</span><img src='https://minotar.net/avatar/"+playerUUIDList[i]+"/32.png' id='playerHeadImage' alt='Skin head'></img><br><hr>";
+            }
+        }else{
+            currentPlayerCount = 0;
+        }
+        document.getElementById("currentPlayerCount").innerHTML = currentPlayerCount;
+
+    }catch(TypeError){
+        currentPlayerCount = 0;
+        document.getElementById("currentPlayerCount").innerHTML = currentPlayerCount;
+    }
+
+    setTimeout(getPlayerInfo, 1000);
+}
