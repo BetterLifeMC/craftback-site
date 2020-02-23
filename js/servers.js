4e1616049fd39857ac7ef85522ca43822e976043
Gavin C. Pease <gcpease@gt3ch1.tk>
2020-02-23 10:09:44 -0700

    Update servers.js

--- js/servers.js
+++ js/servers.js
@@ -1,3 +1,18 @@
-$.getJSON('servers.json', function(data) {
-    console.log(data);
-});
+servers =
+{
+    "servers" : [
+        {
+            "fingerprint":"1234567890" , "hostname":"games01-serv", "port":"8080"
+        }
+    ]
+}
+
+var serverPorts = [];
+var serverHostnames = [];
+var serverFingerPrints = [];
+
+for (var i = 0; i < servers["servers"].length; i++) {
+    serverPorts.push(servers["servers"][i].port);
+    serverHostnames.push(servers["servers"][i].hostname);
+    serverFingerPrints.push(servers["servers"][i].fingerprint);
+}
