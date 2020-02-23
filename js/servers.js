314b8da2787b93e79cff40ae72e9a8c995d75849
Gavin C. Pease <gcpease@gt3ch1.tk>
2020-02-23 10:16:33 -0700

    Work on making the servers show up

--- js/servers.js
+++ js/servers.js
@@ -2,7 +2,10 @@ servers =
 {
     "servers" : [
         {
-            "fingerprint":"1234567890" , "hostname":"games01-serv", "port":"8080"
+            "fingerprint":"1234567890",
+            "hostname":"games01-serv",
+            "port":"8080",
+            "name":"BungeeCord"
         }
     ]
 }
@@ -15,4 +18,12 @@ for (var i = 0; i < servers["servers"].length; i++) {
     serverPorts.push(servers["servers"][i].port);
     serverHostnames.push(servers["servers"][i].hostname);
     serverFingerPrints.push(servers["servers"][i].fingerprint);
+}
+
+function addServersToPage(){
+    for (var i = 0; i < servers["servers"].length; i++) {
+        document.getElementById("serverDropdownList").innerHTML +=
+            '<a href="servers.html?'+servers["servers"][i].fingerprint+
+            '" class="w3-bar-item w3-button"> '+servers["servers"][i].name+'</a>';
+    }
 }
