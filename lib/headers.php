85e239be10ab2ec8c4939c2ffca5ce32a42085a5
Gavin C. Pease <gcpease@gt3ch1.tk>
2020-02-23 10:01:03 -0700

    Commented out most of the PHP

--- lib/headers.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8">
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-metro.css">
-<script src="https://kit.fontawesome.com/1be2cd7175.js" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="js/sketch.js"></script>
