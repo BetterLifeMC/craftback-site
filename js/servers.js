2e9ca7dba0b6a0ffb6d57bc3c893b152422ba35a
Gavin C. Pease <gcpease@gt3ch1.tk>
2020-02-23 10:33:45 -0700

    Add servers from json to main page

--- js/servers.js
+++ js/servers.js
@@ -4,8 +4,9 @@ servers =
         {
             "fingerprint":"1234567890",
             "hostname":"games01-serv",
-            "port":"8080",
-            "name":"BungeeCord"
+            "port":"8083",
+            "name":"BungeeCord",
+            "maxplayers":"-1"
         }
     ]
 }
@@ -20,10 +21,30 @@ for (var i = 0; i < servers["servers"].length; i++) {
     serverFingerPrints.push(servers["servers"][i].fingerprint);
 }
 
-function addServersToPage(){
+function addServersToNav(){
     for (var i = 0; i < servers["servers"].length; i++) {
         document.getElementById("serverDropdownList").innerHTML +=
             '<a href="servers.html?'+servers["servers"][i].fingerprint+
             '" class="w3-bar-item w3-button"> '+servers["servers"][i].name+'</a>';
+    }
+}
+function addServersToMainPage(){
+    element = document.getElementById("bigcontainer");
+    for (var i = 0; i < servers["servers"].length; i++) {
+        if(i % 3 == 0){
+            element.innerHTML +="            <div class='info'>"+
+"                <h2>CHANGEME</h2>"+
+"                <hr>"+
+"                <span class='serverInfo'>Server hostname: <span style='float:right'>"+serverHostnames[i]+"</span></span>"+
+"                <br>"+
+"                <br>"+
+"                <span class='serverInfo'>Max players: <span style='float:right'>"+servers["servers"][i].maxplayers+"</span></span>"+
+"                <br>"+
+"                <br>"+
+"                <span class='serverInfo'>Current players: <span style='float:right' id='currentPlayerCount"+i+"'> </span></span>"+
+"                <br>"+
+"                <br>"+
+"            </div>";
+        }
     }
 }
